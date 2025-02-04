e0adfd9cec482262883d3099d04b0b9b3c14e7ad
Roman Soltan <romabandit88@gmail.com>
2025-02-04 19:49:10 +0200

    add homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
   </StrictMode>,
 );
